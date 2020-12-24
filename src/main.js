c46b6f6bc817fab55f75bcf2afe16731fafdc8e6
Alexey197 <margadonnn3@gmail.com>
2020-12-24 19:57:18 +0300

    реализуем логику load-more-button

--- src/main.js
+++ src/main.js
@@ -8,10 +8,14 @@ import {createTaskEditTemplate} from './components/task-edit.js'
 import {createLoadMoreButtonTemplate} from './components/load-more-button'
 
 const TASK_COUNT = 22
+const SHOWING_TASK_COUNT_ON_START = 8
+const SHOWING_TASK_COUNT_BY_BUTTON = 8
 
 const render = (container, template, place = `beforeend`) => {
   container.insertAdjacentHTML(place, template)
 }
+
+const tasks = generateTasks(TASK_COUNT)
 
 const siteMainElement = document.querySelector(`.main`)
 const siteHeaderElement = siteMainElement.querySelector(`.main__control`)
@@ -22,16 +26,23 @@ render(siteMainElement, createFilterTemplate(filters))
 render(siteMainElement, createBoardTemplate())
 
 const tasksListElement = siteMainElement.querySelector(`.board__tasks`)
-const tasks = generateTasks(TASK_COUNT)
 render(tasksListElement, createTaskEditTemplate(tasks[0]))
 
-// new Array(TASK_COUNT)
-//   .fill(``)
-//   .forEach(
-//       () => render(tasksListElement, createTaskTemplate())
-//   )
-
-tasks.slice(1).forEach((task) => render(tasksListElement, createTaskTemplate(task)))
+let showingTaskCount = SHOWING_TASK_COUNT_ON_START
+tasks.slice(1, showingTaskCount).forEach((task) => render(tasksListElement, createTaskTemplate(task)))
 
 const boardElement = siteMainElement.querySelector(`.board`)
 render(boardElement, createLoadMoreButtonTemplate())
+
+const loadMoreButton = boardElement.querySelector(`.load-more`)
+loadMoreButton.addEventListener(`click`, () => {
+  const prevTaskCount = showingTaskCount
+  showingTaskCount += SHOWING_TASK_COUNT_BY_BUTTON
+  
+  tasks.slice(prevTaskCount, showingTaskCount)
+    .forEach((task) => render(tasksListElement, createTaskTemplate(task)))
+  
+  if (showingTaskCount >= tasks.length) {
+    loadMoreButton.remove()
+  }
+})
